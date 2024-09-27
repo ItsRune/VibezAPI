ab84d1a58b79c6ced5095137bf021bf35fb7003d
ItsRune <32183185+ItsRune@users.noreply.github.com>
2024-09-27 15:54:16 -0400

    ci

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -5,5 +5,5 @@ set -e
 wally install
 
 # Patch the Wally package link modules to also export Luau type definitions.
-rojo sourcemap default.project.json -o sourcemap.json
-wally-package-types --sourcemap sourcemap.json Packages/
+# rojo sourcemap default.project.json -o sourcemap.json
+# wally-package-types --sourcemap sourcemap.json Packages/
