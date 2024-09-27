6ec87a1e0980a699697af40e9ff30160aac7e3df
ItsRune <32183185+ItsRune@users.noreply.github.com>
2024-09-27 15:32:47 -0400

    ci changes once again...

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -10,7 +10,7 @@ fi
 rojo sourcemap default.project.json -o sourcemap.json
 curl -O https://raw.githubusercontent.com/JohnnyMorganz/luau-lsp/main/scripts/globalTypes.d.lua
 
-luau-lsp analyze --definitions=globalTypes.d.lua --base-luaurc=src/.luaurc \
+luau-lsp analyze --definitions=globalTypes.d.lua --base-luaurc=.luaurc \
     --sourcemap=sourcemap.json --settings=.vscode/settings.json \
     --no-strict-dm-types --ignore Packages/**/*.lua --ignore Packages/**/*.luau \
     src/
